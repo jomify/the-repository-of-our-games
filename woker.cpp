821e82c502dcabce2b292cfa58ce71550c1bc981
Jomify <neothype@163.com>
2025-02-27 17:05:07 +0800

    添加项目文件。

---
+++ woker.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<string>
+#include"woker.h"
+#include<string>
+employee::employee(int id, string name, int did) {
+	this->W_ID = id;
+	this->W_Name = name;
+	this->m_Dept_Id = did;
+}
+
+void employee::showinfo() {
+	cout << "职工编号" << this->W_ID;
+	cout << "\t职工姓名" << this->W_Name;
+	cout<< "\t岗位" << this->getjobname(this->m_Dept_Id);
+	cout<< "\t岗位责任" << "完成责任" << endl;
+}
+string employee::getjobname(int did) {
+	char list[3][9] = {"utopia","glode","icarus"};
+	return list[did];
+}
+master::master(int id, string name, int did){
+	this->W_ID = id;
+	this->W_Name = name;
+	this->m_Dept_Id = did;
+}
+
+void master::showinfo() {
+	cout << "职工编号" << this->W_ID;
+	cout << "\t职工姓名" << this->W_Name;
+	cout << "\t岗位" << this->getjobname(this->m_Dept_Id);
+	cout << "\t岗位责任" << "布置任务" << endl;
+
+}
+string master::getjobname(int did) {
+	char list[3][9] = { "utopia","glode","icarus"};
+	return list[did];
+}
+boss::boss(int id, string name, int did){
+	this->W_ID = id;
+	this->W_Name = name;
+	this->m_Dept_Id = did;
+}
+void boss::showinfo() {
+	cout << "职工编号" << this->W_ID;
+	cout << "\t职工姓名" << this->W_Name;
+	cout << "\t岗位" << this->getjobname(this->m_Dept_Id);
+	cout << "\t岗位责任" << "吃饭" << endl;
+}
+string boss::getjobname(int did) {
+	char list[3][9] = { "utopia","glode","icarus" };
+	return list[did];
+}
+
+
---
+++ 职工管理.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include"wokermaged.h"
+#include"woker.h"
+#include<string>
+#include<string.h>
+using namespace std;
+int main() {
+
+	
+
+
+	//实例化管理者
+	wokermanager wm;
+	int choice = 0;
+	while (true) {
+		
+		wm.showmenu();
+		cin >> choice;
+		switch (choice) {
+		case 0:
+			wm.goaway();
+			break;
+
+		case 1:
+			wm.addwoker();
+			wm.save();
+			break;
+
+		case 2:
+			wm.showthem();
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+		default:
+			system("cls");
+			break;
+		}
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	system("pause");
+	return 0;
+}
